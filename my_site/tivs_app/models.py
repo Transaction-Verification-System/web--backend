ae8ae268b38c0b2aa800fc26997f453677f5bd14
Sachit Khadka <sachitkhadka73@gmail.com>
2024-04-16 20:53:12 +0545

    'serializer and models setup'

--- my_site/tivs_app/models.py
+++ my_site/tivs_app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin,BaseUserManager
+
 
 # Create your models here.
+
+class AppUserManager(BaseUserManager):
+    def create_user(self,email,username,password=None):
+        if not email:
+            raise ValueError('Please enter Email.')
+        if not password:
+            raise ValueError('Please enter Password!')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email,password=password)
+        user.set_password(password)
+        user.save()
+
+        return user
+    
+    def create_superuser(self,email,username,password):
+        if not email:
+            raise ValueError('Please enter Email.')
+        if not password:
+            raise ValueError('Please enter password!')
+
+        user = self.create_user(email,username,password)
+        user.is_superuser = True
+        user.save()
+
+        return user
+    
+class AppUser(AbstractBaseUser,PermissionsMixin):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=254)
+    username = models.CharField(max_length=50)
+    password = models.CharField( max_length=50)   
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS =[username]
+
+    def __str__(self):
+        return self.username
+
+
+
---
+++ my_site/tivs_app/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import *
+from django.contrib.auth import get_user_model,authenticate
+from django.forms import ValidationError
+
+User_Model = get_user_model()
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_Model
+        fields='__all__'
+
+        def create(self,clean_data):
+            user_obj = User_Model.objects.create_user(email=clean_data['email'],username=clean_data['username'],password=clean_data['password'])
+            user_obj.save()
+
+            return user_obj
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+
+    def check(self,clean_data):
+        user = authenticate(email = clean_data['email'],password = clean_data['password'])
+        if not user:
+            return ValidationError('Login Failed')
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_Model
+        fields = ['email','username']    
