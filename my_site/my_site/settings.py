699c276c815d535241b976f9409f96cae9c80944
Sachit Khadka <sachitkhadka73@gmail.com>
2024-06-10 21:12:34 +0545

    allowed host chnages

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)5lp!n34f&pq8dgts+vxy#464adh@+!ygn1lrj*$ig9#0n%t-!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
