fbaaefb17197eec30a8193c213fd4d3671788c74
Sachit56 <sachitkhadka73@gmail.com>
2024-07-12 08:12:00 +0545

    celery broker url change

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -130,7 +130,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG" : {
-            'hosts':[('localhost',6379)],
+            'hosts':[os.environ.get('CELERY_BROKER_URL','redis://localhost:6379')],
         }
     }
 }
