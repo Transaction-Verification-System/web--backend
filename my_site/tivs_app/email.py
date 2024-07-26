1fa900eff1a1dfc3789d7a42f8b2586538a265bf
Sachit56 <sachitkhadka73@gmail.com>
2024-07-27 01:39:55 +0545

    email chnages

--- my_site/tivs_app/email.py
+++ my_site/tivs_app/email.py
@@ -1,12 +1,21 @@
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.template.loader import render_to_string
 
 User = get_user_model()
 def send_fail_mail(user,reason,transaction):
     root_user = User.objects.get(id=user)
     subject = f'Failed Transaction Alert - {transaction}'
-    message = f'Hello {root_user},\n\nTransaction ID: {transaction}\nFailed Reason: {reason}\n\nPlease review the transaction and take necessary action.'
+    print('Reason:',reason)
+    statement = f'{reason}' 
+    # message = f'Hello {root_user},\n\nTransaction ID: {transaction}\nFailed Reason: {reason}\n\nPlease review the transaction and take necessary action.'
     from_email = settings.EMAIL_HOST_USER
     recipient_list = [root_user.email]
-    send_mail(subject, message, from_email, recipient_list)
+
+    html_message = render_to_string('tivs_app/email.html', {
+        'user': root_user,
+        'statement': statement,
+        'transaction': transaction
+    })
+    send_mail(subject, '', from_email, recipient_list, html_message=html_message)
