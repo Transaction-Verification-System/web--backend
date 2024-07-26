0e470032ae708de863e880192176ed8e11f92e67
Sachit56 <sachitkhadka73@gmail.com>
2024-07-26 15:54:42 +0545

    aml_risk addition on socket

--- my_site/tivs_app/utility.py
+++ my_site/tivs_app/utility.py
@@ -3,7 +3,7 @@ from typing import Literal
 class ISocketResponse:
     def __init__(self,verified:bool ,message:str , current_transaction_id: str, next_transaction_id: str, total_transactions_checked: int,
                  total_transactions_left: int, total_transactions_accepted: int, total_transactions_rejected: int,
-                 percentage_of_transactions_processed: float, current_process: Literal['BlackList', 'RulesEngine', 'AI']):
+                 percentage_of_transactions_processed: float, current_process: Literal['BlackList', 'RulesEngine', 'AI'],aml_risk:bool):
         self.verified = verified
         self.message = message
         self.current_transaction_id = current_transaction_id
@@ -14,6 +14,7 @@ class ISocketResponse:
         self.total_transactions_rejected = total_transactions_rejected
         self.percentage_of_transactions_processed = percentage_of_transactions_processed
         self.current_process = current_process
+        self.aml_risk = aml_risk 
 
 
     def to_dict(self):
@@ -25,7 +26,8 @@ class ISocketResponse:
             "totalTransactionsAccepted": self.total_transactions_accepted,
             "totalTransactionsRejected": self.total_transactions_rejected,
             "percentageOfTransactionsProcessed": self.percentage_of_transactions_processed,
-            "currentProcess": self.current_process
+            "currentProcess": self.current_process,
+            "aml_risk":self.aml_risk
         }
         return {
             "verified": self.verified,
